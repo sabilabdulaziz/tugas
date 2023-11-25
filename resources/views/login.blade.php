e175cde4070d2671418727f3f91e5012d701b879
yuzu <sabilabdulaziz95@gmail.com>
2023-11-26 00:00:11 +0700

    up

---
+++ resources/views/login.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="row">
+        <div class="col">
+            <img src="logo.jpg" alt="gambar" id="image">
+        </div>
+        <div class="col-md-4">
+            <div class="container">
+                    <h6><img id="ico" src="logo.jpg" alt="ico">Enterstream</h6>
+                    <h1 id="h-1">Login</h1>
+                    <form action="#" action="post" id="form">
+                        <div class="row">
+                            <div class="col">
+                                <label for="email">email</label>
+                                <input type="text" class="form-control" name="email">
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col ">
+                                <label for="password">password</label>
+                                <input type="password" class="form-control" name="password">
+                            </div>
+                        </div>
+                        <a href="#" class="link mt-3" id="li-1"> Lupa password</a>
+                        <div class="row mt-4">
+                            <div class="col">
+                                <input type="submit" name="kirim" id="send" value="masuk" class="btn btn-primary">
+                            </div>
+                        </div>
+                        <div id="li-2">Tidak Punya akun ? <a href="">Daftar disini !</a></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+</html>
